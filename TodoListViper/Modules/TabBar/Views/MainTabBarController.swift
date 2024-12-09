c4c2d1fa8e3b21723ee8eebf592425d5328b22f1
omercsx <omercagris@gmail.com>
2024-12-09 13:03:38 -0500

    Main tabbar controller

--- TodoListViper/Modules/TabBar/Views/MainTabBarController.swift
+++ TodoListViper/Modules/TabBar/Views/MainTabBarController.swift
@@ -5,3 +5,16 @@
 //  Created by Omer Cagri Sayir on 9.12.2024.
 //
 
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAppearance()
+    }
+
+    private func setupAppearance() {
+        tabBar.tintColor = .systemGreen
+        tabBar.backgroundColor = .systemBackground
+    }
+}
